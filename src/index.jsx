f08a1511d053e9772486ce412ed5a479797a20ed
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2018-04-20 07:37:00 +0300

    Removed unneeded stuff

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,10 @@ import React, { Component } from "react";
 import sample from "./sample.ogg";
 
 class SpringAudio extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <audio autoplay="true" preload="auto">
         <source src={sample} type="audio/ogg" />
-        Your browser does not support the audio element.
       </audio>
     );
   }
